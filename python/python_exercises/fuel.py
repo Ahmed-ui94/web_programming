a68cd39a011c1149f8866e7519024ff31abd23be
Ahmed Abukar <ahabukar466@gmail.com>
2022-06-20 11:37:29 +0300

    fuel calculation program

---
+++ python/python_exercises/fuel.py
@@ -0,0 +1,27 @@
+
+def main():
+    while True:
+        try: 
+            fractions = input("fractions: ").split("/")
+            x = int(fractions[0])
+            y = int(fractions[1])
+            if y == 0:
+                raise ZeroDivisionError
+        except (ValueError, ZeroDivisionError):
+            continue
+        else:
+            return fuel_percent(x, y)
+    
+#function to convert fraction to percentage
+def fuel_percent(x, y):
+    percent = (x/y) * 100
+    if percent >= 99:
+        print("F")
+    elif percent <= 1:
+        print("E")
+    else:
+        print(int(percent),'%',sep="")
+
+
+if __name__ == "__main__":
+    main()
