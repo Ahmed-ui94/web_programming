3cbbbb53d04623e57e8d99081ac903c9946b61d0
Ahmed Abukar <ahabukar466@gmail.com>
2022-08-11 07:52:36 +0300

    added a comment

--- python/python_exercises/mysum.py
+++ python/python_exercises/mysum.py
@@ -1,9 +1,15 @@
-def mysum(*arg):
+def mysum(arg):
     if not arg:
         return arg
     output = arg[0]
     for i in arg[1:]:
-        output += i
+        if i.is_integer():
+            output += i
+        elif i.isalnum():
+            i = int(i)
+            output += i
+        else:
+            continue
     return output
 
-print(mysum((1,2,3,4),(1,3,4,4)))
+print(mysum([38.0, 20.0, 30.0, "30", "f"]))
