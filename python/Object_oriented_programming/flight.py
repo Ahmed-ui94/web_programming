baad1673691e64d9bd131478778b74e20c926b69
Ahmed Abukar <ahabukar466@gmail.com>
2022-10-14 08:56:04 +0300

    flight module modified to be more of a responsive

--- python/Object_oriented_programming/flight.py
+++ python/Object_oriented_programming/flight.py
@@ -2,18 +2,20 @@ class Flight:
     def __init__(self, capacity):
         self.capacity = capacity
         self.passanger = []
+   
+   
     # add passanger to the flight
     def add_passanger(self,name):
         self.name = name
-        if len(self.passanger) > self.capacity:
+        if len(self.passanger) < self.capacity:
             self.passanger.append(name)
         else:
-            print("No available seat")
+            print(f"Not available seat for {name}")
+
 
 flight = Flight(3)
 peoples = ["Ahmed","Abde","ali","farah"]
 for people in peoples:
-    flight.name = people
-    flight.add_passanger(flight.passanger)
+    flight.add_passanger(people)
 
 print(flight.passanger)
