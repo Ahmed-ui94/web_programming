64a6b5c6c62d2326280a86dd844db338b5e7a58c
Ahmed Abukar <ahabukar466@gmail.com>
2022-11-26 10:10:57 +0300

    calender dmodified

--- python/python_exercises/calender.py
+++ python/python_exercises/calender.py
@@ -4,10 +4,7 @@ year = int(input("Enter the year: (1901 to 2099): ") )
 month = int(input("choose a month from: "))
 day = input("choose a day from that month: ")
 
-#mon = calendar.TextCalendar()
-#cal = fmon.formatmonth(year, month, w=3)
 
-#print(cal.replace('\n','\n').replace(''+day,'*'+day, 1))
 
 def calenderformater():
     fmon = calendar.TextCalendar()
